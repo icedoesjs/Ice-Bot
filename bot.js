fd274b1018b2c7dc2b8edc6b1866a9a8e32cb623
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2019-09-23 22:23:21 -0400

    New SQL items, events and sql files were added

--- bot.js
+++ bot.js
@@ -8,18 +8,13 @@ const fetch = require('node-fetch');
 const config = require("./config.json");
 const {CommandHandler} = require('djs-commands');
 const requireAll = require('require-all');
-const lc = require('letter-count');
 const prefix = config.prefix;
 const db = require('quick.db');
-var color = "#0000FF";
-var version = "Beta 1"
-
-//Database Configs
-db.set('serverInfo.filter', {toggled: 'true'}) //By diffult the bad word filter is toggled on, this database has upcoming plans
-db.set('userInfo.money', 100)
-db.set('userInfo.level', 0)
-
-
+var color = config.color;
+var version = config.version;
+var numcommands = config.commands
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
 
 //Command Handler
 const CH = new CommandHandler({
@@ -50,13 +45,31 @@ if (message.member.type === 'bot') return undefined;
   let cmd = CH.getCommand(command);
 if (cmd) {
     try {
-        await cmd.run(client, message, args, config, Discord, lc, prefix, db, version, color); //Add vars here
+        await cmd.run(client, message, args, config, prefix, db, version, color); //Add vars here
     } catch (error) {
+        console.error(error)
         await console.error(`Error: ${error}`);
         await console.error(`Error Message: ${error.message}`);
     }
 }
 });
+
+client.on("ready", () => {
+  // Check if the table "points" exists.
+  const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+  if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
+  }
+
+    // And then we have two prepared statements to get and set the score data.
+    client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+    client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+    });
 
 client.on('ready', () => { //On ready
  console.log(`\u001b[31m`, `------------[ Ice Bot ]------------`)
@@ -78,10 +91,111 @@ client.on("guildCreate", guild => { //Reset activity on Join
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
  client.user.setActivity(`i/help | Watching ${client.guilds.size} servers`);
+ const embed = new Discord.MessageEmbed()
+   .setTitle("Thank you for adding me!")
+   .setColor(config.color)
+   .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+   .setDescription(`Thank you for adding me to your guild, you can see all my commands by doing i/help. \n I currently am being actively developed so expect changes and improvements. \n If you would like welcome messages please name your welcome channel welcomes. \n If you encounter any errors please don 't hesitate to join [our Discord](https://discord.gg/qRdNb9D)!`)
+   .addField("Stats:", `Guilds: ${client.guilds.size} Users: ${client.users.size}`, true)
+   .addField("Quick Links:", `[Github](https://github.com/iceyym8/Ice-Bot) [Invite](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot) [Support](https://discord.gg/qRdNb9D) [Vote](https://bots.ondiscord.xyz)`, true)
+   .setColor(config.color)
+   .setFooter(`Thank you for using Ice bot, I was made by IceyyM8#0816 | Active Development Phase | Commands: ${numcommands}`)
+   const joinchannel = guild.channels.find(channel => channel.name === "general");
+   joinchannel.send(embed)
+   console.log("The embed on join was sent!")
 });
+
+
 
 client.on("guildDelete", guild => { //Reset activity on leave
   // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id}), all database files were removed!`);
  client.user.setActivity(`i/help | Watching ${client.guilds.size} servers`);
 })
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  let score;
+  if (message.guild) {
+    score = client.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = {
+        id: `${message.guild.id}-${message.author.id}`,
+        user: message.author.id,
+        guild: message.guild.id,
+        points: 0,
+        level: 1
+      }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if (score.level < curLevel) {
+      score.level++;
+      const embed = new Discord.MessageEmbed()
+      .setAuthor(client.user.username)
+      .setTitle(`a user ranked up!`)
+      .setDescription(`${message.author.username} ranked up to rank ${curLevel}!`)
+      .setTimestamp()
+      message.channel.send(embed)
+    }
+    client.setScore.run(score);
+  }
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // Command-specific code here!
+});
+
+const invites = {};
+
+// A pretty useful method to create a delay without blocking the whole script.
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  // "ready" isn't really ready. We need to wait a spell.
+  wait(1000);
+
+  // Load all invites for all guilds and save them to the cache.
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    var welctxt = db.get('serverInfo.wtxt')
+    if (welctxt === undefined) {
+      db.set('serverInfo.wtxt', `Welcome to ${member.guild.name} please enjoy your stay here.`)
+    }
+    if (invite === undefined) {
+      let (invite.code = "None")
+      let (inviter.tag = "None")
+      let (invite.uses = "None")
+    }
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    const welcomechannel = member.guild.channels.find(channel => channel.name === "welcomes");
+    const welcomeEmbed = new Discord.MessageEmbed()
+      .setColor('#0000FF')
+      .setTitle(`Welcome ${member.user.username} to ${member.guild.name}`)
+      .setDescription(`${welctxt}`)
+      .setThumbnail(member.user.avatarURL)
+      .addField(`Invite Code Used:`, `discord.gg/${invite.code}`, true)
+      .addField(`Invited by:`, `${inviter.tag}`, true)
+      .addField(`Invite uses:`, `${invite.uses}`, true)
+      .setFooter(`Thank you for using Ice Bot | Member count: ${member.guild.memberCount}`)
+      .setTimestamp()
+    welcomechannel.send(welcomeEmbed)
+  });
+});
